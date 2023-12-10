751127987e8ab1830562288e413eaee34ca00772
shivamgupta002 <shivamgupta992909@gmail.com>
2023-12-10 17:50:36 +0530

    Connect Database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+import express from "express";
+import dotenv from "dotenv";
+
+import Connection from "./database/db.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const USERNAME = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+Connection(USERNAME, PASSWORD);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
