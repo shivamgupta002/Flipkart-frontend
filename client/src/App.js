66dce52194850069ed0817049f2ff5e8a0b61dc0
shivamgupta002 <shivamgupta992909@gmail.com>
2023-12-10 16:05:09 +0530

    Relocate Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./Header/Header";
+import { Box } from "@mui/material";
+// import Home from "./Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Box style={{ marginTop: "54px" }}>
+        {/* <Home /> */}
+      </Box>
     </>
   );
 }
