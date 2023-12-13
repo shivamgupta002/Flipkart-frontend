b43fa630774bd6f8b5f8bfe4becb3098f83c7dc9
shivamgupta002 <shivamgupta992909@gmail.com>
2023-12-13 15:26:15 +0530

    Move constants data from frontend to backend

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -19,3 +19,62 @@ export const bannerData = [
     { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
     { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' }
 ]
+
+
+// const timerURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg';
+
+// const adURL = 'https://rukminim1.flixcart.com/flap/464/708/image/633789f7def60050.jpg?q=70';
+
+// export const imageURL = [
+//     'https://rukminim1.flixcart.com/flap/960/960/image/2f30db9425df5cec.jpg?q=50',
+//     'https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg',
+//     'https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50'
+// ];
+
+// const url = 'https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50';
+
+// const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
+
+// const adURL = 'https://rukminim1.flixcart.com/lockin/774/185/images/CCO__PP_2019-07-14.png?q=50';
+
+// const imgurl = 'https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90';
+
+// function isDate(val) {
+//     // Cross realm comptatible
+//     return Object.prototype.toString.call(val) === '[object Date]'
+// }
+
+// function isObj(val) {
+//     return typeof val === 'object'
+// }
+
+// function stringifyValue(val) {
+//     if (isObj(val) && !isDate(val)) {
+//         return JSON.stringify(val)
+//     } else {
+//         return val
+//     }
+// }
+
+// function buildForm({ action, params }) {
+//     const form = document.createElement('form')
+//     form.setAttribute('method', 'post')
+//     form.setAttribute('action', action)
+
+//     Object.keys(params).forEach(key => {
+//         const input = document.createElement('input')
+//         input.setAttribute('type', 'hidden')
+//         input.setAttribute('name', key)
+//         input.setAttribute('value', stringifyValue(params[key]))
+//         form.appendChild(input)
+//     })
+
+//     return form
+// }
+
+// export function post(details) {
+//     const form = buildForm(details)
+//     document.body.appendChild(form)
+//     form.submit()
+//     form.remove()
+// }
