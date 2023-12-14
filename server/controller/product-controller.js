f7d3cadd10f096fc7392d3ec69005addcfc07bfa
shivamgupta002 <shivamgupta992909@gmail.com>
2023-12-14 15:50:30 +0530

    product Detail backend

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -8,3 +8,14 @@ export const getProducts = async (req, res) => {
     return res.status(500).json(e.message);
   }
 };
+
+export const getProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findOne({ 'id': id });
+    res.status(200).json(product);
+  } catch (e) {
+    console.log("Error find in Get Product By ID  due to ", error.message);
+    return res.status(500).json(e.message);
+  }
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { userSignUp, userLogin } from "../controller/user-controller.js";
-import { getProducts } from "../controller/product-controller.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controller/product-controller.js";
 
 const router = express.Router();
 
 router.post("/signUp", userSignUp);
 router.post("/login", userLogin);
 
-router.get('/products',getProducts)
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
 
 export default router;
